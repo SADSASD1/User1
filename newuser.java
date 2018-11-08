6c3380343dc10546332a7eea363ba7f18d60a12e
SADSASD1 <aleqsanyan1996@.ru>
2018-11-08 14:47:03 +0400

    User

---
+++ newuser.java
@@ -0,0 +1,18 @@
+public class newuser {
+    private String Firstname;
+private String Lastname;
+private String Email;
+private String Password;
+    public  void main(String[] args) {
+User us=new User();
+this.Firstname="Enter your Firstname";
+this.Lastname="Enter your LastName";
+this.Email="Enter your Email";
+this.Password="Enter your Password";
+        System.out.println(this.Firstname);
+        System.out.println(this.Lastname);
+        System.out.println(this.Email);
+        System.out.println(this.Password);
+    }
+
+}
